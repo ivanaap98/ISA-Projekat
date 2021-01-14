342c904a2cb452bca422a2375efe9f80c63048cd
milanm98 <62839581+milanm98@users.noreply.github.com>
2021-01-14 16:20:55 +0100

    react

--- amin/frontend/src/App.js
+++ amin/frontend/src/App.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -16,8 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Index page
-
+          Learn React
         </a>
       </header>
     </div>
