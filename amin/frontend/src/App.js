64712dae96c94f6135ae543576ba72320a484dc8
milanm98 <62839581+milanm98@users.noreply.github.com>
2021-01-14 15:24:42 +0100

    Update App.js

--- amin/frontend/src/App.js
+++ amin/frontend/src/App.js
@@ -15,7 +15,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Index page
+
         </a>
       </header>
     </div>
