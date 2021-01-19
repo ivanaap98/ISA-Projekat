2f960fc8ff83de885bf1a78734e1b1ea6abe0b3c
andjelijaa <andjelijaa@users.noreply.github.com>
2021-01-19 13:20:43 +0100

    Front

--- amin/frontend/src/HomePage/Login/Login.js
+++ amin/frontend/src/HomePage/Login/Login.js
@@ -4,9 +4,9 @@ export default function Login() {
   return (
    	<form method="GET">
             <label for="email">email <span></span></label>
-            <input type="email" name="email" id="email" placeholder="type a valid email"/>
+            <input type="email" name="email" id="email" placeholder="unesi svoj mail"/>
             <label for="pwd">password <span></span></label>
-            <input type="password" name="pwd" id='pwd'placeholder="type a complex password"/>
+            <input type="password" name="pwd" id='pwd'placeholder="unesi svoj password"/>
             
             
             <input type='submit' value='login'/>
--- amin/frontend/src/HomePage/Registracija/Registracija.js
+++ amin/frontend/src/HomePage/Registracija/Registracija.js
@@ -4,14 +4,20 @@ export default function Registracija() {
   return(
  
  	<form method="GET">
-            <label for="userN">username <span></span></label>
-            <input type='text' name= 'username' id ='userN' placeholder="type your name in english please..."/>
+           <label for="email">email <span></span></label>
+            <input type="email" name="email" id="email" placeholder="unesi svoj mail"/>
             <label for="pwd">password <span></span></label>
-            <input type="password" name="pwd" id='pwd'placeholder="type a complex password"/>
-            <label for="email">email <span></span></label>
-            <input type="email" name="email" id="email" placeholder="type a valid email"/>
-            <label for="phone">phone <span></span></label>
-            <input type="text" name="phone" id="phone" placeholder="your phone here please..."/>
+            <input type="password" name="pwd" id='pwd'placeholder="unesi svoju sifru"/>
+            <label for="ime">Ime <span></span></label>
+            <input type="text" name="ime" id="ime" placeholder="upisi svoje ime"/>
+            <label for="prezime">Prezime <span></span></label>
+            <input type="text" name="prezime" id="prezime" placeholder="upisi svoje prezime"/>
+            <label for="phone">Telefon <span></span></label>
+            <input type="text" name="phone" id="phone" placeholder="upisi svoj telefon"/>
+            <label for="adresa">Adresa <span></span></label>
+            <input type="text" name="adresa" id="adresa" placeholder="upisi adresu"/>
+            <label for="grad">Grad <span></span></label>
+            <input type="text" name="grad" id="grad" placeholder="upisi grad"/>
             <input type='submit' value='register'/>
         </form>
  	
