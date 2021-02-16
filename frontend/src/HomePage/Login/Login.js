572c9f6229d3cd21d1184dc57dfd04ae2e4ecedc
milan marjanov <milan@mari.mari>
2021-02-16 17:08:50 -0500

    login page partialy fixed

--- frontend/src/HomePage/Login/Login.js
+++ frontend/src/HomePage/Login/Login.js
@@ -5,20 +5,20 @@ export default function Login() {
   return (
     <main id="background" className="z-0 bg-fixed h-screen w-screen">
       
-      <form className="text-black items-center rounded flex flex-col text-3xl">
+      <form className="bg-gradient-to-r from-blue-300 to-blue-900 w-1/5 content-center font-black text-black items-center rounded flex flex-col text-1xl">
 
-        <div className="mb-4 text-center w-1/3"> 
-          <label for="email">Email :<span></span></label>
-          <input className="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker" type="email" name="email" id="email" placeholder="unesite svoj email"/>
+        <div className="mt-14 mb-4 text-center w-5/6"> 
+          <label for="email" className="text-white">Email : </label>
+          <input className="h-12 mt-3 shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker" type="email" name="email" id="email" placeholder="unesite email"/>
         </div>
 
-        <div className="mb-6 text-center w-1/3">
-          <label for="pwd">Lozinka :</label>
-          <input className="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker" type="password" name="pwd" id='pwd'placeholder="unesite lozinku"/>
+        <div className="mt-3 mb-6 text-center w-5/6">
+          <label for="pwd" className="text-white">Lozinka :</label>
+          <input className="h-12 mt-3 shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker" type="password" name="pwd" id='pwd'placeholder="unesite lozinku"/>
         </div>
 
         <div>
-          <button className="w-1/3 mb-8 items-center justify-between w-32" type='submit' value='login'>Potvrdi</button>
+          <button className="border-gray-600 text-2xl font-black text-white w-1/3 mb-8 items-center justify-between" type='submit' value='login'>Potvrdi</button>
         </div>
 
       </form>
